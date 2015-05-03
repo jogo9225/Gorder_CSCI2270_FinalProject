3ce47c794a58a337a7f2bc2f9efb45698a88b636
Joel Gorder <gorder.joel@gmail.com>
2015-05-03 11:20:05 -0600

    added a rounding change

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <stdlib.h>
+#include <iomanip>
 #include "models.h"
 
 using namespace std;
@@ -41,17 +42,17 @@ int main() {
     double maxPercentChange = myLinkedList->findMaxPercentChange();
     int counterPartsOver20Percent = myLinkedList->countForecastChangeOver20P();
     int countFrom0 = myLinkedList->countForcFrom0();
-
+	//Implemented a rounding change:
 	//This is our output.  The print statments give us the information we wanted out of the data we read in. 
     cout << "The number of parts is: " << counter << endl;
     cout << modelCounter << " of the " << counter << " parts used different models for the two forecasts." << endl;
-    cout << "The average percent change in the amount forecasted between runs is: " << avgPercentChange*100 << "%" << endl;
-    cout << "The average percent change in the forecast when the model did change is: " << avgChanged*100 << "%" << endl;
-    cout << "The average percent change in the forecast when the model remained the same is : " << avgNotChanged*100 << "%" << endl;
-    cout << "The standard deviation of the parts forcast with models that changed is: " << stdvChanged << endl;
-    cout << "The standard deviation of parts forcast with models that remained the same is: " << stdvNotChanged << endl;
-    cout << "The largest change in the number of parts forecasted for a particular part number is: " << maxUnitChange << endl;
-    cout << "The largest percent change in the number of parts forecasted for a particular part number is: " << maxPercentChange*100 << "%" << endl;
+    cout << "The average percent change in the amount forecasted between runs is: " << setprecision(2) << fixed << avgPercentChange*100 << "%" << endl;
+    cout << "The average percent change in the forecast when the model did change is: " << setprecision(2) << fixed << avgChanged*100 << "%" << endl;
+    cout << "The average percent change in the forecast when the model remained the same is : " << setprecision(2) << fixed  << avgNotChanged*100 << "%" << endl;
+    cout << "The standard deviation of the parts forcast with models that changed is: " << setprecision(2) << fixed  << stdvChanged << endl;
+    cout << "The standard deviation of parts forcast with models that remained the same is: " << setprecision(2) << fixed  << stdvNotChanged << endl;
+    cout << "The largest change in the number of parts forecasted for a particular part number is: " << setprecision(0) << fixed  << maxUnitChange << endl;
+    cout << "The largest percent change in the number of parts forecasted for a particular part number is: " << setprecision(2) << fixed  << maxPercentChange*100 << "%" << endl;
     cout << "The number of forecasts that changed by more than 20% is: " << counterPartsOver20Percent << endl;
     cout << "The number of parts that went from 0 forecast in run 1 to having a forecast in run 2 is: " << countFrom0 << endl;
 
